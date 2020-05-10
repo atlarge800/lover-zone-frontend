ba47b6711066f99749fa9a4b4f3ecc9abe2c5433
dxkite <dxkite@163.com>
2020-05-10 21:53:15 +0800

    初始化迁移

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  metaInfo: {
+    titleTemplate: "%s | 至我的爱人",
+    link: [
+      { rel: "favicon", href: "./assets/favicon.ico" },
+      { rel: "apple-touch-icon", href: "./assets/favicon.png", sizes: "57x57" },
+      { rel: "apple-touch-icon", href: "./assets/favicon.png", sizes: "72x72" },
+      {
+        rel: "apple-touch-icon",
+        href: "./assets/favicon.png",
+        sizes: "114x114"
+      },
+      {
+        rel: "apple-touch-icon",
+        href: "./assets/favicon.png",
+        sizes: "144x144"
+      },
+      {
+        rel: "shortcut icon",
+        href: "./assets/favicon.ico",
+        type: "image/x-icon"
+      }
+    ]
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <div class="top-text">我们相爱已经{{days}}天啦~</div>
+    <div class="container">
+      <div class="mobie heart heart-beat">
+        <div class="heart heart-shadow"></div>
+      </div>
+    </div>
+    <div class="bottom-text">
+      <div class="name">
+        <i class="iconfont icon-boy"></i>
+        {{boy}}
+      </div>
+      <i class="iconfont icon-love"></i>
+      <div class="name">
+        <i class="iconfont icon-girl"></i>
+        {{girl}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+import "@/fonts/iconfont.css";
+import "@/css/heart.css";
+import '@/js/utils/snow'
+
+export default {
+  name: "Home",
+  metaInfo() {
+    return {
+      title: this.title
+    };
+  },
+  data() {
+    return {
+      boy: "冯世昌",
+      girl: "胡青青",
+      time: "2020-02-14 20:00:00"
+    };
+  },
+  computed: {
+    days: function() {
+      return moment(new Date()).diff(moment(new Date(this.time)), "days");
+    },
+    title: function() {
+      return "相爱的第" + this.days + "天";
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
